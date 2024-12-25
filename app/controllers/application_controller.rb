b7b28c522cde917e4f11832c673b6da037c0a067
Raymond Hughes <raymond.hughes@live.com>
2024-12-24 21:48:43 -0500

    Adding an user registration system with custom layout based on if a user is currently logged in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_layout
+
+  def set_layout
+    @layout = user_signed_in? ? "application" : "authentication"
+  end
+
+  private
+
+  def user_signed_in?
+    !!Current.session
+  end
 end
